e631b72eeebc53d2628e1274db43e124d7798b65
parksoobin <soobin9894@naver.com>
2016-11-02 12:40:00 +0900

    폴더 생성 기능 추가

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.ansan.myapplication;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,19 +25,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View v){
+        if(isStoragePermissionGranted()==false){
+
+            Toast.makeText(getApplicationContext(),"SD Card 사용불가",Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String folder = path + "/myLoveDir";
+        String filename = folder+"/myfile.txt";
+
+        File myfolder = new File(folder);
+
+
         switch (v.getId()){
             case R.id.button1 : //폴더생성
-
+                myfolder.mkdir();
+                Toast.makeText(getApplicationContext(),"폴더 생성 완료",Toast.LENGTH_SHORT).show();
                 break;
+
             case R.id.button2 : //폴더삭제
 
                 break;
+
             case R.id.button3 : //파일생성
 
                 break;
+
             case R.id.button4 : //파일읽기
 
                 break;
+
             case R.id.button5 : //파일목록가져오기
 
                 break;
